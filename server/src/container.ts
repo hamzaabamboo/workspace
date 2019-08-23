cf86d9b9bfd5f2575831def4bc94f604bd22e46f
himkwtn <him10424@gmail.com>
2019-08-23 13:50:15 +0700

    remove old server

--- server/src/container.ts
+++
@@ -1,6 +0,0 @@
-import "reflect-metadata";
-import Container from "typedi";
-import { ClassResolvers } from "./types";
-
-export const getResolvers = classResolvers =>
-  Container.get<ClassResolvers>(classResolvers).resolvers();
--- server/src/user/auth.permisisons.ts
+++
@@ -1,9 +0,0 @@
-import { isAuthenticated } from "./auth.rules";
-
-const authPermissions = {
-  Query: {
-    currentUser: isAuthenticated
-  }
-};
-
-export default authPermissions;
