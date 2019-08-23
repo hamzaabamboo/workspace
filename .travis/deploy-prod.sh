4c0925aee1ebcf3cfb40c583b81efa1e3ff1a79f
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2019-08-23 10:55:16 +0700

    :wrench: better script

--- .travis/deploy-prod.sh
+++ .travis/deploy-prod.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" $REGISTRY_URL --password-stdin
 
 docker push $REGISTRY_URL/workspace/nginx
 docker push $REGISTRY_URL/workspace/backend
 docker push $REGISTRY_URL/workspace/client
 
 echo "ssh to server"
-ssh deploy@$HOST "cd workspace/production && git pull origin master && ./scripts/deploy-prod.sh"
+# ssh deploy@$HOST "cd workspace/production && git pull origin master && ./scripts/deploy-prod.sh"
--- .travis/deploy-staging.sh
+++ .travis/deploy-staging.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" $REGISTRY_URL --password-stdin
 
 docker tag $REGISTRY_URL/workspace/nginx $REGISTRY_URL/workspace/nginx:staging
 docker tag $REGISTRY_URL/workspace/backend $REGISTRY_URL/workspace/backend:staging
@@ -11,4 +10,4 @@ docker push $REGISTRY_URL/workspace/backend
 docker push $REGISTRY_URL/workspace/client
 
 echo "ssh to server"
-ssh deploy@$HOST "cd workspace/staging && git pull origin staging && ./scripts/deploy-staging.sh"
+# ssh deploy@$HOST "cd workspace/staging && git pull origin staging && ./scripts/deploy-staging.sh"
