bc3c00b719598059638bb70bb7586f018cc201d0
himkwtn <him10424@gmail.com>
2019-08-30 13:36:58 +0700

    update docker file

--- server/src/generate-typing.ts
+++ server/src/generate-typing.ts
@@ -8,5 +8,4 @@ const definitionsFactory = new GraphQLDefinitionsFactory();
 definitionsFactory.generate({
   typePaths: ['./src/**/*.graphql'],
   path: join(process.cwd(), 'src/graphql.ts'),
-  // watch: process.env.NODE_ENV !== 'production',
 });
